b81ea8fc10c110eff3d403f22fb19ddf0cc7f043
Marsel.Abdrakhmanov <marsel.abdrakhmanov@easycomm.ru>
2023-03-25 14:06:26 +0300

    Initial Commit

---
+++ TaskTrackerWidget/TaskTrackerWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerWidgetApp.swift
+//  TaskTrackerWidget
+//
+//  Created by Марсель on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
